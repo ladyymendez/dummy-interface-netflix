545479d7d137b9caade36a949d60ac8d8ba85e70
ladyymendez <ladyy.mendez@gmail.com>
2020-08-27 15:50:30 -0500

    Row: Fetch movies and adding css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Row from './components/Row';
 import { requestMovies } from './shared';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Row title="Netflix Originals"
         fetchURL={
           requestMovies.fetchNetflixOriginals
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,8 +1,12 @@
 import React, {useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 import { url } from '../shared';
+import './Row.css';
 
-function Row({title, fetchURL}) {
-  const [movie, setMovies] = useState([]);
+const baseUrl = "https://image.tmdb.org/t/p/original";
+
+function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
@@ -14,10 +18,32 @@ function Row({title, fetchURL}) {
   }, [fetchURL]);
 
   return (
-    <div>
-       <h2>{title}</h2>
+    <div className="row">
+      <h2>{title}</h2>
+      <div className="row_posters">
+        {
+        movies.map((movie) => (
+          <img className="row_poster"
+            key={movie.id}
+            src={
+              `${baseUrl}${
+                movie.poster_path
+              }`
+            }
+            alt={
+              movie.name
+            }/>
+        ))
+        }
+      </div>
     </div>
   );
 }
+
+Row.propTypes = {
+  title: PropTypes.string.isRequired,
+  fetchURL: PropTypes.string.isRequired
+};
+
 
 export default Row;

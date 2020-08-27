5890622256429b93c28d08dcb4855effc9ea89e5
ladyymendez <ladyy.mendez@gmail.com>
2020-08-27 16:06:08 -0500

    Nav: Added nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Row from './components/row/Row';
 import Banner from './components/banner/Banner';
+import Nav from './components/nav/Nav';
 import { requestMovies } from './shared';
 import './App.css';
 
 function App() {
   return (
     <div className="app">
+      <Nav />
       <Banner />
       <Row title="Netflix Originals"
         fetchURL={
---
+++ src/components/nav/Nav.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+import './Nav.css';
+
+function Nav() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll",() =>{
+      if(window.scrollY > 100){
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+    return () => window.removeEventListener("scroll");
+  },[])
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img className="nav_logo" src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="Netflix Logo"/>
+      <img className="nav_avatar" src="https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-portrait-176256935.jpg" alt="Avatar"/>
+    </div>
+  );
+}
+
+export default Nav;
